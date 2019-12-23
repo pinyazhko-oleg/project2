72939dc3e6dd440b506f3f49d6e064ef127d4124
pinyazhko-oleg <pinyazchko.oleg@gmail.com>
2019-12-23 21:14:29 +0200

    Applying axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,38 +1,25 @@
 import React from 'react';
-import styles from './users.module.css'
+import styles from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png';
 
 let Users = (props) => {
-  if (props.users.length === 0) {
-  props.setUsers (
-    [
-      { id: 1,
-        photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSzrbLOhZLKXFdU9YjpUzQ5F6yXEtBKGqSopAbV6Ef5bqc1oxnX',
-        followed: false,
-        fullName: 'Fry',
-        status: 'boss',
-        location: {city: 'Lviv', country: 'Ukraine'} },
-      { id: 2,
-        photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSzrbLOhZLKXFdU9YjpUzQ5F6yXEtBKGqSopAbV6Ef5bqc1oxnX',
-        followed: true,
-        fullName: 'Andriy',
-        status: 'boss',
-        location: {city: 'Krakow', country: 'Poland'} },
-      { id: 3,
-        photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSzrbLOhZLKXFdU9YjpUzQ5F6yXEtBKGqSopAbV6Ef5bqc1oxnX',
-        followed: false,
-        fullName: 'Igor',
-        status: 'boss',
-        location: {city: 'Minsk', country: 'Belarus'} },
-    ]
-  );
-}
+
+    let getUsers = () => {
+    if (props.users.length === 0) {
+      axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+        props.setUsers(response.data.items)
+      });
+    }
+  }
 
   return <div>
+          <button onClick={getUsers}>Get Users</button>
       {
         props.users.map(u => <div key={u.id}>
           <span>
             <div>
-              <img src={u.photoUrl} className={styles.userPhoto}/>
+              <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
             </div>
             <div>
               {u.followed
@@ -42,12 +29,12 @@ let Users = (props) => {
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              <div>{'u.location.country'}</div>
+              <div>{'u.location.city'}</div>
             </span>
           </span>
 
